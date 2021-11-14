9e378338a44f94869aeb50e462a34201ec34c22f
Alisa-Mikulina <alice.mikulina@gmail.com>
2021-11-14 16:29:58 +0300

    Usermodels added

--- models/UserModel.py
+++ models/UserModel.py
@@ -1,24 +1,33 @@
+from typing import Optional
 from pydantic import BaseModel, Field
 from pydantic.networks import EmailStr
 
 from models.BaseModel import BaseModelWithId, BaseModelWithIdConfig
 
 class UserBase(BaseModel):
-	username: str = Field(...)
+	username: str = Field(min_length=8, max_length=50)
 
 class UserLoginReq(BaseModel):
-	username: str = Field(...)
-	password: str = Field(...)
+	username: str = Field(min_length=8, max_length=50)
+	password: str = Field(min_length=8, max_length=50)
 	fingerPrint: str = Field(...)
 
 class UserLoginRes(BaseModel):
 	token: str = Field(...)
 
 class UserRegisterReq(UserBase):
-	password: str = Field(...)
+	password: str = Field(min_length=8, max_length=50)
+	name: str = Field(min_length=1, max_length=50)
+	lastname: str = Field(min_length=1, max_length=50)
+	contact: str = Field(min_length=3,max_length=50)
+	information: str = Field(min_length=20,max_length=500)
 
 class UserInDB(BaseModelWithId, UserBase):
+	name: str = Field(default='')
+	lastname: str = Field(default='')
+	contact: str = Field(default='')
+	information: str = Field(default='')
 	password_hash: str = Field(default='')
 
 	class Config(BaseModelWithIdConfig):
-		schema_extra = {"example": {"username": "My goodName", "password_hash": "adhahduad123u1"}}
+		schema_extra = {"example": {"username": "My goodName", "name": "MyName", "lastname": "MyLastname", "contact": "MyContact", "password_hash": "adhahduad123u1"}}

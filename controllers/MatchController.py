f109da3e6a2c94a8ad52fc72124f35c0f29da9e2
Alisa-Mikulina <alice.mikulina@gmail.com>
2021-11-29 19:21:20 +0300

    endpoints/feature some bugs fixed

--- controllers/MatchController.py
+++ controllers/MatchController.py
@@ -1,36 +1,26 @@
 from typing import List, Optional
 from pymongo.database import Database
-from models.UserModel import UserInDB
-from models.ProjectModel import ProjectInDB
 from models.MatchModel import MatchInDB
 
-def createMatch(db: Database, match: MatchInDB, username: str, slug: str, likeFromUser: Optional[bool], likeFromProject: Optional[bool]):
-    newMatch = MatchInDB(**{**match.dict(), 'username': username, 'slug': slug, 'likeFromUser': likeFromUser, 'likeFromProject': likeFromProject})
-    db.matches.insert_one(newMatch.dict())
 
 def getMatchByBoth(db: Database, username: str, slug: str):
-    match = MatchInDB.matches.find_one({'slug': slug}, {'username': username})
+    match = db.matches.find_one({'slug': slug}, {'username': username})
     if match:
-        return MatchInDB(**match)
+        return db(**match)
 
-def addUserLikeToMatch(db: Database, username: str, slug: str):
-    match = getMatchByBoth(db, username, slug)
-    if match:
-        match.likeFromUser = True
-    return
-
-def addProjectLikeToMatch(db: Database, username: str, slug: str):
-    match = getMatchByBoth(db, username, slug)
-    if match:
-        match.likeFromProject = True
-    return
+def createOrUpdateMatch(db: Database, username: str, slug: str, likeFromUser: Optional[bool], likeFromProject: Optional[bool]):
+    predictMatch = getMatchByBoth(db, username, slug)
+    if predictMatch:
+            db.matches.find_one_and_update({'username': username}, {'slug': slug}, {'likeFromUser': likeFromUser}, {'likeFromProject': likeFromProject})
+            pass
+    db.matches.insert_one({'username': username, 'slug': slug, 'likeFromUser': likeFromUser, 'likeFromProject': likeFromProject})
 
 def getUserMatches(db: Database, username: str):
     matches = db.matches.find({'username': username})
     if matches:
-        return list(map(lambda ob: ProjectInDB(**ob), matches))
+        return list(map(lambda ob: MatchInDB(**ob), matches))
 
 def getProjectMatches(db: Database, slug: str):
     matches = db.matches.find({'slug': slug})
     if matches:
-        return list(map(lambda ob: ProjectInDB(**ob), matches))
+        return list(map(lambda ob: MatchInDB(**ob), matches))
--- routers/MatchRouter.py
+++ routers/MatchRouter.py
@@ -4,7 +4,7 @@ from pymongo.database import Database
 from controllers.ProjectController import getProjectBySlug
 from controllers.TokenController import getAuthorizedUser
 from controllers.UserController import getUserByUsername
-from controllers.MatchController import createMatch, getMatchByBoth, addUserLikeToMatch, addProjectLikeToMatch, getUserMatches, getProjectMatches
+from controllers.MatchController import createOrUpdateMatch, getMatchByBoth, getUserMatches, getProjectMatches
 from db.mongodb import getDatabase
 from models.MatchModel import MatchBase, MatchInDB, MatchCreateReq
 from models.ProjectModel import ProjectInDB
@@ -13,36 +13,30 @@ from models.UserModel import UserInDB
 matchRouter = APIRouter(prefix='/match', tags=['match'])
 
 @matchRouter.post('/user_like', status_code=status.HTTP_201_CREATED, response_model=MatchBase)
-async def create(slug: ProjectInDB.slug = Depends(getProjectBySlug), 
-                 username: UserInDB.username = Depends(getAuthorizedUser),
+async def create(project: ProjectInDB = Depends(getProjectBySlug), 
+                 user: UserInDB = Depends(getAuthorizedUser),
                  match: MatchCreateReq = Body(...),
                  db: Database = Depends(getDatabase)):
-	predictMatch = getMatchByBoth(db, username, slug)
-	if predictMatch:
-        if predictMatch.likeFromProject:
-            if predictMatch.likeFromUser:
-                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                                    detail='Match already exists')
-            else:
-                addUserLikeToMatch(db, username, slug)
-    createMatch(db, match, username, slug, likeFromUser=True)
-	return {**match.dict(), 'slug': slug, 'username': username}
+        slug = project.slug
+        username = user.username
+        predictMatch = getMatchByBoth(db, username, slug)
+        if predictMatch and predictMatch.likeFromProject and predictMatch.likeFromUser:
+                return
+        createOrUpdateMatch(db, username, slug, likeFromUser=True)
+        return {**match.dict(), 'slug': slug, 'username': username}
 
 @matchRouter.post('/project_like', status_code=status.HTTP_201_CREATED, response_model=MatchBase)
-async def create(slug: ProjectInDB.slug = Depends(getProjectBySlug), 
-                 username: UserInDB.username = Depends(getAuthorizedUser),
+async def create(project: ProjectInDB = Depends(getProjectBySlug), 
+                 user: UserInDB = Depends(getAuthorizedUser),
                  match: MatchCreateReq = Body(...),
                  db: Database = Depends(getDatabase)):
-	predictMatch = getMatchByBoth(db, username, slug)
-	if predictMatch:
-        if predictMatch.likeFromUser:
-            if predictMatch.likeFromProject:
-                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                                    detail='Match already exists')
-            else:
-                addProjectLikeToMatch(db, username, slug)
-    createMatch(db, match, username, slug, likeFromProject=True)
-	return {**match.dict(), 'slug': slug, 'username': username}
+        slug = project.slug
+        username = user.username
+        predictMatch = getMatchByBoth(db, username, slug)
+        if predictMatch and predictMatch.likeFromUser and predictMatch.likeFromProject:
+                return
+        createOrUpdateMatch(db, username, slug, likeFromProject=True)
+        return {**match.dict(), 'slug': slug, 'username': username}
 
 @matchRouter.get('/my_matches', status_code=status.HTTP_201_CREATED, response_model=List[MatchBase])
 async def selfMatches(db: Database = Depends(getDatabase), user: UserInDB = Depends(getAuthorizedUser)):
